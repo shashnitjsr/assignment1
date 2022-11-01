fe18d1450f3d002be03cdaddf1a7ba3580929041
Shashwat Sinha <shashwat.sinha3@flipkart.com>
2022-11-01 19:21:51 +0530

    Adding UserStore

--- trelloclone/src/main/java/enitites/User.java
+++ trelloclone/src/main/java/enitites/User.java
@@ -4,12 +4,27 @@ public class User {
     private String id;
     private long lastVisitedTimeInMillis;
 
-
     public User(String id, long lastVisitedTimeInMillis) {
         this.id = id;
         this.lastVisitedTimeInMillis = lastVisitedTimeInMillis;
     }
 
     public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getLastVisitedTimeInMillis() {
+        return lastVisitedTimeInMillis;
+    }
+
+    public void setLastVisitedTimeInMillis(long lastVisitedTimeInMillis) {
+        this.lastVisitedTimeInMillis = lastVisitedTimeInMillis;
     }
 }
---
+++ trelloclone/src/main/java/store/UserStore.java
@@ -0,0 +1,27 @@
+package store;
+
+import enitites.User;
+import java.util.HashMap;
+import java.util.Map;
+
+//Singleton class
+public class UserStore {
+   Map<String, User> userMap = new HashMap<>();
+   private static UserStore userStore = new UserStore();
+
+   private UserStore() {
+
+   }
+
+   public static UserStore getInstance() {
+       return userStore;
+   }
+
+   public void addUser(User user) {
+       userMap.put(user.getId(), user);
+   }
+
+   public User getUser(String id) {
+       return userMap.get(id);
+   }
+}
